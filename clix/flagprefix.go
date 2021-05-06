70d16059e3327ba007537da30c45539e0662e2c5
takumakei <takumakei@gmail.com>
2021-05-06 17:58:28 +0900

    update clix.FlagPrefix

--- clix/flagprefix.go
+++ clix/flagprefix.go
@@ -12,12 +12,22 @@ func (fp FlagPrefix) String() string {
 	return string(fp)
 }
 
-// EnvVars returns []string{FlagPrefix + s}.
-func (fp FlagPrefix) EnvVars(s string) []string {
-	return []string{string(fp) + s}
+// EnvVars returns []string{FlagPrefix + s, ...}.
+func (fp FlagPrefix) EnvVars(s ...string) []string {
+	a := make([]string, len(s))
+	for i, v := range s {
+		a[i] = string(fp) + v
+	}
+	return a
 }
 
-// FilePath returns clix.FilePath(os.Getenv(FlagPrefix + s + "_FILE")).
-func (fp FlagPrefix) FilePath(s string) string {
-	return FilePath(os.Getenv(string(fp) + s + "_FILE"))
+// FilePath returns clix.FilePath(os.Getenv(FlagPrefix + s + "_FILE"), ...).
+func (fp FlagPrefix) FilePath(s ...string) string {
+	for _, v := range s {
+		v = FilePath(os.Getenv(string(fp) + v + "_FILE"))
+		if len(v) > 0 {
+			return v
+		}
+	}
+	return ""
 }
